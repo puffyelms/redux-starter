330e079e109b044a39508ac7ee93121ede2f0e4a
Tim Getz <timgetz@gmail.com>
2016-09-14 20:18:45 -0400

    Finished Async Status and Error Handling Section

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -3,6 +3,7 @@ import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
 import CourseForm from './CourseForm';
+import toastr from 'toastr';
 
 class ManageCoursePage extends React.Component {
   constructor(props, context) {
@@ -10,7 +11,8 @@ class ManageCoursePage extends React.Component {
 
     this.state = {
       course: Object.assign({}, this.props.course),
-      errors: {}
+      errors: {},
+      saving: false
     };
     this.updateCourseState = this.updateCourseState.bind(this);
     this.saveCourse = this.saveCourse.bind(this);
@@ -30,10 +32,21 @@ class ManageCoursePage extends React.Component {
     return this.setState({course: course});
   }
 
+  redirect() {
+    this.setState({saving: false});
+    toastr.success('Course saved');
+    this.context.router.push('/courses');
+  }
   saveCourse(event) {
     event.preventDefault();
-    this.props.actions.saveCourse(this.state.course);
-    this.context.router.push('/courses');
+    this.setState({saving: true});
+    this.props.actions.saveCourse(this.state.course)
+      .then(() => this.redirect())
+      .catch(error => {
+        toastr.error(error);
+        this.setState({saving: false});
+      });
+
   }
 
   render() {
@@ -43,7 +56,9 @@ class ManageCoursePage extends React.Component {
         onChange={this.updateCourseState}
         onSave={this.saveCourse}
         course={this.state.course}
-        errors={this.state.errors} />
+        errors={this.state.errors}
+        saving={this.state.saving}
+      />
     );
   }
 }
